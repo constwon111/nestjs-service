efa9733f9ce9e83e5e84a1d61b3f55d8151c1208
taekywong Won <constwon111@gmail.com>
2022-06-30 19:09:03 +0900

    [New] validatioinSchema.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
