1b23e74ae788718f3d8cd33cba9ae57cb9437644
taekywong Won <constwon111@gmail.com>
2022-07-03 23:58:15 +0900

    [Modified] replace nest logger to winston

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,7 @@ import {
   Get,
   HttpException,
   Inject,
+  LoggerService,
   Param,
   Query,
 } from '@nestjs/common';
@@ -17,32 +18,32 @@ import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserInfo } from './UserInfo';
 import { UsersService } from './users.service';
 import { WinstonLogger, WINSTON_MODULE_PROVIDER } from 'nest-winston';
-
+import { InternalServerErrorException } from '@nestjs/common';
 @Controller('/users')
 export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
-    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: WinstonLogger,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: LoggerService,
   ) {}
 
   @Post()
   async createUser(@Body() dto: CreateUserDto): Promise<void> {
     const { name, email, password } = dto;
-    this.printWinstonLog(dto);
+    this.printLoggerServiceLog(dto);
     await this.usersService.createUser(name, email, password);
   }
 
-  private printWinstonLog(dto) {
-    // console.log(this.logger.name);
-
-    this.logger.error('error: ', dto);
-    this.logger.warn('warn: ', dto);
-    // this.logger.info('info: ', dto);
-    // this.logger.http('http: ', dto);
-    this.logger.verbose('verbose: ', dto);
-    this.logger.debug('debug: ', dto);
-    // this.logger.silly('silly: ', dto);
+  private printLoggerServiceLog(dto) {
+    try {
+      throw new InternalServerErrorException('test');
+    } catch (e) {
+      this.logger.error('error: ' + JSON.stringify(dto), e.stack);
+    }
+    this.logger.warn('warn: ' + JSON.stringify(dto));
+    this.logger.log('log: ' + JSON.stringify(dto));
+    this.logger.verbose('verbose: ' + JSON.stringify(dto));
+    this.logger.debug('debug: ' + JSON.stringify(dto));
   }
 
   @Post('/email-verify')
