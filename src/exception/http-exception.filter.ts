a3de443d0cb01c4957f50ff555d0363ff8b427ce
taekywong Won <constwon111@gmail.com>
2022-07-04 15:56:46 +0900

    [Feature] exception filter added

---
+++ src/exception/http-exception.filter.ts
@@ -0,0 +1,36 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Logger } from 'winston';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private logger: Logger) {}
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+    const req = ctx.getRequest<Request>();
+    const stack = exception.stack;
+
+    if (!(exception instanceof HttpException)) {
+      exception = new InternalServerErrorException();
+    }
+
+    const response = (exception as HttpException).getResponse();
+
+    const log = {
+      timestamp: new Date(),
+      url: req.url,
+      response,
+      stack,
+      level: 'verbose',
+      message: 'ajs',
+    };
+    this.logger.log(log);
+    res.status((exception as HttpException).getStatus()).json(response);
+  }
+}
