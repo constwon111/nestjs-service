9126e83786c98496094c458900e0fa26056dc2eb
taekywong Won <constwon111@gmail.com>
2022-07-08 22:00:07 +0900

    [Modified] migration done

---
+++ src/migrations/1657284908769-CreateUserTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUserTable1657284908769 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
